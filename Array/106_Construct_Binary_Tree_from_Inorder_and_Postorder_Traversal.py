918e9466dc5b4e8d1118da52e73997608ff762d2
xiejie <519399762@qq.com>
2018-12-15 11:31:04 +0800

    modify

--- Array/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ Array/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -18,6 +18,7 @@ Return the following binary tree:
    15   7
 """
 # 由中序遍历和后序遍历确定树结构
+# 后续遍历的最后一个节点为根节点，但是对后续数组从后往前遍历时，先遇到的时右子树
 
 
 # Definition for a binary tree node.
@@ -34,3 +35,16 @@ class Solution(object):
         :type postorder: List[int]
         :rtype: TreeNode
         """
+        if inorder:
+            index = inorder.index(postorder.pop())
+            root = TreeNode(inorder[index])
+
+            root.right = self.buildTree(inorder[index+1:], postorder)
+            root.left = self.buildTree(inorder[:index], postorder)
+
+            return root
+
+
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+print(Solution().buildTree(inorder, postorder))
