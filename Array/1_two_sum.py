048a67816cb60286c8e03969374dc5db847aebf8
xiejie <519399762@qq.com>
2018-12-17 13:06:42 +0800

    add

--- Array/1_two_sum.py
+++ Array/1_two_sum.py
@@ -1,4 +1,3 @@
-#1
 """
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
@@ -11,7 +10,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
-
+#22
 # Time:  O(n)
 # Space: O(n)
 
