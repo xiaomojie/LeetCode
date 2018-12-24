0769c520c7329701c70906166a02591238b3afec
xiejie <519399762@qq.com>
2018-12-24 15:18:48 +0800

    hash table

--- Hash Table/349_Intersection_of_Two_Arrays.py
+++ Hash Table/349_Intersection_of_Two_Arrays.py
@@ -1,8 +1,23 @@
+class Solution:
+    def intersection2(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set_s = set(nums1)
+        set_t = set(nums2)
+        res = []
+        # for i, x in enumerate(set_s):
+        for x in set(set_s):
+            if x in set_t:
+                res.append(x)
+        return res
 
-set_s = set()
-set_t = set()
-res = []
-for i in len(set_s):
-    if set_s[i] in set_t:
-        res.append(set_s[i])
-return res
+    def intersection(self, nums1, nums2):
+        return list(set(nums1)&set(nums2))
+
+    def intersection3(self, nums1, nums2):
+        return list(x for x in set(nums1) if x in set(nums2))
+
+
