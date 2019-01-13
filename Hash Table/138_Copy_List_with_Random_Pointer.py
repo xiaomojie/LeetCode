0ce3f1e7bffb094ad36dc4f345e5c1e1617c7efc
xiejie <519399762@qq.com>
2019-01-13 20:05:29 +0800

    hash table

---
+++ Hash Table/138_Copy_List_with_Random_Pointer.py
@@ -0,0 +1,41 @@
+"""
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+"""
+
+# Definition for singly-linked list with a random pointer.
+import collections
+
+
+class RandomListNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+
+class Solution(object):
+    def copyRandomList1(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        # O(2n)
+        dic = dict()
+        m, n = head
+        while m:
+            dic[m] = RandomListNode(m.label)
+            m = m.next
+        while n:
+            dic[n].next = dic[n.next]
+            dic[n].random = dic[n.random]
+            n = n.next
+        return dic.get(head)
+
+    def copyRandomList(self, head):
+        # O(n)
+        dic = collections.defaultdict(lambda: RandomListNode(0))
+        dic[None] = None
+        n = head
+        while n:
