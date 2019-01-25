0e6ec1cd92ace8aabe705c8e0770e048941a441b
xiejie <519399762@qq.com>
2019-01-25 11:29:29 +0800

    linked list

--- Linked List/707_Design_Linked_List.py
+++ Linked List/707_Design_Linked_List.py
@@ -8,7 +8,6 @@ addAtHead(val) : Add a node of value val before the first element of the linked 
 addAtTail(val) : Append a node of value val to the last element of the linked list.
 addAtIndex(index, val) : Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
 deleteAtIndex(index) : Delete the index-th node in the linked list, if the index is valid.
-
 Example:
 
 MyLinkedList linkedList = new MyLinkedList();
@@ -18,20 +17,24 @@ linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
 linkedList.get(1);            // returns 2
 linkedList.deleteAtIndex(1);  // now the linked list is 1->3
 linkedList.get(1);            // returns 3
-Note:
-
-All values will be in the range of [1, 1000].
-The number of operations will be in the range of [1, 1000].
-Please do not use the built-in LinkedList library.
 """
 
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        self.pre = None
 
-class MyLinkedList:
-
+class MyLinkedList(object):
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.head = ListNode(-1)
+        self.tail = ListNode(-1)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+        self.size = 0
 
     def get(self, index):
         """
@@ -39,6 +42,15 @@ class MyLinkedList:
         :type index: int
         :rtype: int
         """
+        if index < 0 or index >= self.size:
+            return -1
+        else:
+            cur = self.head.next
+            while index:
+                cur = cur.next
+                index -= 1
+            return cur.val
+
 
     def addAtHead(self, val):
         """
@@ -46,6 +58,13 @@ class MyLinkedList:
         :type val: int
         :rtype: void
         """
+        node = ListNode(val)
+        node.next = self.head.next
+        self.head.next.pre = node
+        self.head.next = node
+        node.pre = self.head
+        self.size += 1
+
 
     def addAtTail(self, val):
         """
@@ -53,6 +72,12 @@ class MyLinkedList:
         :type val: int
         :rtype: void
         """
+        node = ListNode(val)
+        self.tail.pre.next = node
+        node.pre = self.tail.pre
+        node.next = self.tail
+        self.tail.pre = node
+        self.size += 1
 
     def addAtIndex(self, index, val):
         """
@@ -61,6 +86,19 @@ class MyLinkedList:
         :type val: int
         :rtype: void
         """
+        if index < 0 or index > self.size:
+            return -1
+        else:
+            node = ListNode(val)
+            cur = self.head
+            while index:
+                cur = cur.next
+                index -= 1
+            node.next = cur.next
+            cur.next.pre = node
+            cur.next = node
+            node.pre = cur
+
 
     def deleteAtIndex(self, index):
         """
@@ -68,12 +106,23 @@ class MyLinkedList:
         :type index: int
         :rtype: void
         """
+        if index < 0 or index >= self.size:
+            return -1
+        else:
+            cur = self.head
+            while index:
+                cur = cur.next
+                index -= 1
+            cur.next = cur.next.next
+            cur.next.pre = cur
 
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
 
+
+
+        # Your MyLinkedList object will be instantiated and called as such:
+        # obj = MyLinkedList()
+        # param_1 = obj.get(index)
+        # obj.addAtHead(val)
+        # obj.addAtTail(val)
+        # obj.addAtIndex(index,val)
+        # obj.deleteAtIndex(index)
--- Linked List/725_Split_Linked_List_in_Parts.py
+++ Linked List/725_Split_Linked_List_in_Parts.py
@@ -1,12 +1,13 @@
-# 先统计长度，然后计算好每一段的长度，例如10，k = 3，则分配为[4,3,3]
-# 然后外层循环为range(3), 内层循环为[4,3,3],在必要的时候掐断
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
+    # 先统计长度，然后计算好每一段的长度，例如10，k = 3，则分配为[4,3,3]
+    # 然后再循环,在必要的时候掐断
     def splitListToParts(self, root, k):
         """
         :type root: ListNode
@@ -18,10 +19,17 @@ class Solution:
         while cur:
             cur, count = cur.next, count+1
         avg = count//k
-        leave = count % k
-        parts = [avg + 1] * leave + [avg] * (k-leave)
-        res = []
-        cur = root
-        for i in parts:
-            while i and cur:
-                i -= 1
+        left = count % k
+        parts = [avg + 1] * left + [avg] * (k-left)
+        res = [0]*len(parts)
+        for index, item in enumerate(parts):
+            cur = head = ListNode(0)
+            head.next = root
+            while item:
+                cur = cur.next
+                item -= 1
+            root = cur.next
+            cur.next = None
+            res[index] = head.next
+        return res
+
