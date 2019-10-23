eedf73b5f167025a97f0905d3718b6eab2ee3e09
xiejie <519399762@qq.com>
2019-10-23 18:41:06 +0800

    2019-10-23

---
+++ Tree/450. 删除二叉搜索树中的节点( Delete Node in a BST).py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left or not root.right:
+                root = root.left if root.left else root.right
+            else:
+                cur = root.right
+                while cur.left:
+                    cur = cur.left
+                root.val = cur.val
+                root.right = self.deleteNode(root.right, cur.val)
+        return root
